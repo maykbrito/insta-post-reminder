3e5dc35d011dbaf243523afb4919b45b754b0672
Mayk Brito <maykbrito@gmail.com>
2021-02-26 20:12:57 -0300

    final da primeira aula

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { Notifyer } from './Notifyer.js';
+
+const App = {
+  async start() {
+   try {
+     await Notifyer.init()
+     Notifyer.notify({
+       title: "Hora do Post",
+       body: "Crie algum conteúdo para ajudar a comunidade"
+     })
+   } catch (err) {
+     console.log(err.message)
+   }
+  }
+}
+
+export { App }
---
+++ src/Notifyer.js
@@ -0,0 +1,16 @@
+const Notifyer = {
+  async init() {
+    const permission = await Notification.requestPermission()
+    if( permission !== "granted") {
+      throw new Error('Permissão negada')
+    }
+  },
+  notify({ title, body, icon }) {
+    new Notification(title, {
+      body,
+      icon
+    })
+  }
+}
+
+export { Notifyer }
